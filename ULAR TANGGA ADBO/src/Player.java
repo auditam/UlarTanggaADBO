d9619deb11e50ba235af13151320e7b07fe9be92
priambodo_pangestu <priambodo_pangestu@192.168.0.102>
2015-11-17 15:18:27 +0700

    Add a Board Class

---
+++ ULAR TANGGA ADBO/src/Player.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author priambodo_pangestu
+ */
+public class Player {
+    
+    //atribut untuk nama satu pemain
+    private String nama;
+    
+    //atribut untuk posisi yang dimiliki pemain
+    private int posisi;
+    
+    //status pemain apakah menang (1) atau tidak/belum (0)
+    private boolean status;
+    
+    
+    /**
+     * method yang digunakan untuk mengatur posisi pemain
+     */
+    public void setPosisi(){
+        
+    }
+    
+    /**
+     * method yang digunakan untuk mendapatkan posisi pemain tertentu
+     * @return posisi pemain
+     */
+    public int getPosisi(){
+        return this.posisi;
+    }
+}
